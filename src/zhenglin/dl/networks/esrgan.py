138222a0d8eebd236a3d6218ad86fbd6d3b1a1aa
Zhenglin <zhengli3@ualberta.ca>
2023-07-03 20:51:11 +0800

    esrgan: patchGAN Discriminator version: 0.13.5

--- src/zhenglin/dl/networks/esrgan.py
+++ src/zhenglin/dl/networks/esrgan.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 from torchvision.models import vgg19
-import math
+import numpy as np
 
 
 class FeatureExtractor(nn.Module):
@@ -94,29 +94,33 @@ class GeneratorRRDB(nn.Module):
 
 
 class Discriminator(nn.Module):
-    def __init__(self, input_shape):
+    def __init__(self, channels, patch_scale=1):
         super(Discriminator, self).__init__()
+        if patch_scale & (patch_scale - 1) != 0:
+            raise ValueError("Patch scale should be a power of 2")
 
-        self.input_shape = input_shape
-        in_channels, in_height, in_width = self.input_shape
-        # patch_h, patch_w = int(in_height / 2 ** 4), int(in_width / 2 ** 4)
-        self.output_shape = (1, in_height, in_width)
-
-        def discriminator_block(in_filters, out_filters, first_block=False):
+        def discriminator_block(in_filters, out_filters, first_block=False, downsample=False):
             layers = []
             layers.append(nn.Conv2d(in_filters, out_filters, kernel_size=3, stride=1, padding=1))
             if not first_block:
                 layers.append(nn.BatchNorm2d(out_filters))
             layers.append(nn.LeakyReLU(0.2, inplace=True))
-            layers.append(nn.Conv2d(out_filters, out_filters, kernel_size=3, stride=1, padding=1))
+            if downsample:
+                layers.append(nn.Conv2d(out_filters, out_filters, kernel_size=3, stride=2, padding=1))
+            else:
+                layers.append(nn.Conv2d(out_filters, out_filters, kernel_size=3, stride=1, padding=1))
             layers.append(nn.BatchNorm2d(out_filters))
             layers.append(nn.LeakyReLU(0.2, inplace=True))
             return layers
 
+        down = [0, 0, 0, 0]
+        for i in range(np.log2(patch_scale).astype(int)):
+            down[i] = 1
+            
         layers = []
-        in_filters = in_channels
+        in_filters = channels
         for i, out_filters in enumerate([64, 128, 256, 512]):
-            layers.extend(discriminator_block(in_filters, out_filters, first_block=(i == 0)))
+            layers.extend(discriminator_block(in_filters, out_filters, first_block=(i == 0), downsample=down[i]))
             in_filters = out_filters
 
         layers.append(nn.Conv2d(out_filters, 1, kernel_size=3, stride=1, padding=1))

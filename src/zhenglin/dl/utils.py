cb6b7c73af705b910332b7886b1be4b95e4cf6ca
Zhenglin <zhengli3@ualberta.ca>
2023-07-04 20:14:37 +0800

    replaybuffer version:0.13.6

--- src/zhenglin/dl/utils.py
+++ src/zhenglin/dl/utils.py
@@ -1,4 +1,5 @@
 import torch
+from networks.cyclegan import ReplayBuffer as ReplayBuffer_
 
 def summary(model):
     print(model)
@@ -15,3 +16,28 @@ def fix_model(model):
     model.eval()
     
     return model
+
+class ReplayBuffer(ReplayBuffer_):
+    pass
+
+class EasyReplayBuffer:
+    def __init__(self, max_size=10):
+        """
+            An easier implementation of replaybuffer with peek()
+            TODO: using list could be slow, use tensor concate instead
+        """
+        assert max_size > 0
+        self.max_size = max_size
+        self.data = []
+    
+    def push_and_pop(self, data):
+        if len(self.data) < self.max_size:
+            self.data.append(data)
+        else:
+            self.data.pop(0)
+            self.data.append(data)
+        return self.data[-1]
+    
+    def peek(self):
+        assert len(self.data) != 0
+        return self.data[0]

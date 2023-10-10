b643670ba5444122f25c450c1e0f31b379fb2c8b
Zhenglin <zhengli3@ualberta.ca>
2023-10-10 16:49:16 -0600

    overhaul of infer

--- src/zhenglin/dl/templates/v1/infer.py
+++ src/zhenglin/dl/templates/v1/infer.py
@@ -16,17 +16,14 @@ from network import Generator
 from dataset import MyDataset
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--start_epoch', type=int, default=0, help='starting epoch')
-parser.add_argument('--end_epoch', type=int, default=200, help='number of epochs of training')
-parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
 parser.add_argument('--dataroot', type=str, default='', help='root directory of the dataset')
-parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate')
-parser.add_argument('--decay_epoch', type=int, default=100, help='epoch to start linearly decaying the learning rate to 0')
-parser.add_argument('--size', type=int, default=256, help='size of the data crop (squared assumed)')
+parser.add_argument('--model_path', type=str, default='./data/test', help='path of trained model')
+parser.add_argument('--infer_epoch', type=int, default=0, help='epoch num of infer')
+parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
+parser.add_argument('--patch_size', type=int, default=512, help='size of the data crop (squared assumed)')
 parser.add_argument('--input_nc', type=int, default=3, help='number of channels of input data')
 parser.add_argument('--output_nc', type=int, default=3, help='number of channels of output data')
-parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
-parser.add_argument('--resume', action="store_true", help='continue training from a checkpoint')
+parser.add_argument('--num_workers', type=int, default=8, help='number of cpu threads to use during batch generation')
 args = parser.parse_args()
 print(args)
 
@@ -35,6 +32,7 @@ DEVICE = 0
 
 ### Networks
 model = Generator().to(DEVICE)
+model.load_state_dict(torch.load(args.model_path, map_location=torch.device(DEVICE)))
 
 ### if rich
 # model = nn.DataParallel(model, device_ids=[0, 1])
@@ -42,8 +40,8 @@ model = Generator().to(DEVICE)
 model.eval()
 
 ### Inputs & targets memory allocation
-Tensor = torch.cuda.FloatTensor if args.cuda else torch.Tensor
-input_A = Tensor(args.batch_size, args.input_nc, args.size, args.size)
+Tensor = torch.cuda.FloatTensor
+input_A = Tensor(args.batch_size, args.input_nc, args.patch_size, args.patch_size)
 
 ### Dataset loader
 transforms_ = [ transforms.Resize(int(args.size*1.12), Image.BICUBIC), 
@@ -52,10 +50,10 @@ transforms_ = [ transforms.Resize(int(args.size*1.12), Image.BICUBIC),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)) ]
 dataset = MyDataset(args.dataroot, transforms_=transforms_, unaligned=True)
-dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.n_cpu)
+dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 
 ######## infer ######
-for epoch in range(args.start_epoch, args.end_epoch + 1):
+for epoch in range(args.infer_epoch):
     for i, batch in enumerate(dataloader):
         
         Input = Variable(input_A.copy_(batch))

b8d9a4185b8d8600513257b23aa79c32019e7e78
Zhenglin <zhengli3@ualberta.ca>
2024-03-15 16:51:02 -0600

    cancel init

--- src/zhenglin/__init__.py
+++ src/zhenglin/__init__.py
@@ -1,59 +1,59 @@
-"""
-Use *from zhenglin import * to import all commonly used pakages
-outside of this project. However, for safety, DON NOT use 
-*import zhenglin* with in THIS project since it goes against
-the principle of isolation and if not, could cause unexpected errors.
-"""
+# """
+# Use *from zhenglin import * to import all commonly used pakages
+# outside of this project. However, for safety, DON NOT use 
+# *import zhenglin* with in THIS project since it goes against
+# the principle of isolation and if not, could cause unexpected errors.
+# """
 
-# system packages
-import os
-import sys
-import shutil
-import time
-import argparse
-import itertools
+# # system packages
+# import os
+# import sys
+# import shutil
+# import time
+# import argparse
+# import itertools
 
-# torch packages
-import torch
-import torch.onnx
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import DataLoader, Dataset
-from torch.autograd import Variable
-from torchvision.transforms import transforms
-from torchvision.utils import save_image
-from torch.cuda.amp import GradScaler, autocast
+# # torch packages
+# import torch
+# import torch.onnx
+# import torch.nn as nn
+# import torch.nn.functional as F
+# from torch.utils.data import DataLoader, Dataset
+# from torch.autograd import Variable
+# from torchvision.transforms import transforms
+# from torchvision.utils import save_image
+# from torch.cuda.amp import GradScaler, autocast
 
-# dl ml packages
-import cv2
-import h5py
-import pickle
-import numpy as np
-import pandas as pd
-import random as rd
-import scipy as sp
-import skimage
-import sklearn
-from PIL import Image
-import albumentations
+# # dl ml packages
+# import cv2
+# import h5py
+# import pickle
+# import numpy as np
+# import pandas as pd
+# import random as rd
+# import scipy as sp
+# import skimage
+# import sklearn
+# from PIL import Image
+# import albumentations
 
-# universal packages
-import math as m
-import copy
-from glob import glob
-from tqdm import tqdm
-from collections import OrderedDict, defaultdict
-import matplotlib.pyplot as plt
-import seaborn as sns
-import wandb
-import typing
-import json
-import yaml
-import re
+# # universal packages
+# import math as m
+# import copy
+# from glob import glob
+# from tqdm import tqdm
+# from collections import OrderedDict, defaultdict
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import wandb
+# import typing
+# import json
+# import yaml
+# import re
 
-# quantization packages
-# import onnx
-# import torch2trt
-# from torch2trt import TRTModule
-# import pycuda.driver as cuda
-# import pycuda.autoinit
+# # quantization packages
+# # import onnx
+# # import torch2trt
+# # from torch2trt import TRTModule
+# # import pycuda.driver as cuda
+# # import pycuda.autoinit

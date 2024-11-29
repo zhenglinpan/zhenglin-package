c5bdece842187bb3321bcaae60fd35e47e5afd6a
zhenglin <zhengli3@ualberta.ca>
2024-11-29 18:21:24 +0800

    update pytorch training template

--- src/zhenglin/pytorch/dataset.py
+++ src/zhenglin/pytorch/dataset.py
@@ -1,13 +1,44 @@
+import cv2
+import datasets
+import numpy as np
+import albumentations as A
 from torch.utils.data import Dataset
-from torchvision.transforms import transforms
+from albumentations.pytorch import ToTensorV2
 
-class MyDataset(Dataset):
-    def __init__(self, trans=None, *args):
-        self.trans = transforms.Compose(trans)
-        self.data = []
-        
-    def __getitem__(self, index):
-        data = self.trans(self.data[index]) # data: ndarray ([C, H, W])
-    
+class MNIST(Dataset):
     def __init__(self):
-        return len(self.files)
+        super().__init__()
+        self.mnist = datasets.load_dataset("mnist", split="train")
+        self.transform = A.Compose([A.HorizontalFlip(p=0.5), 
+                                    ToTensorV2()], 
+                                   additional_targets={"contour": "image"})
+
+    def __len__(self):
+        return len(self.mnist)
+
+    def __getitem__(self, index):
+        img = np.array(self.mnist[index]["image"])    # PIL image
+        label = self.mnist[index]["label"]
+        countour = cv2.Canny(img, 100, 200)
+        
+        transformed = self.transform(image=img, contour=countour)
+        img = transformed["image"] / 255.
+        countour = transformed["contour"] / 255.
+        
+        return {"image": img, "contour": countour, "label": label}
+        
+
+if __name__ == "__main__":
+    from torch.utils.data import DataLoader
+    from torchvision.utils import save_image
+    
+    dataset = MNIST()
+    dataloader = DataLoader(dataset, batch_size=16, shuffle=False, num_workers=4)
+    
+    for batch in dataloader:
+        image = batch["image"]
+        contour = batch["contour"]
+        
+        save_image(image, "image.png")
+        save_image(contour, "contour.png")
+        break
--- src/zhenglin/pytorch/train.py
+++ src/zhenglin/pytorch/train.py
@@ -1,108 +1,108 @@
-import os, sys
+import os
+import sys
+import logging
+import datetime
 import argparse
-import itertools
 from tqdm import tqdm
 
 import torch
-import torch.nn as nn
 from torch.utils.data import DataLoader
-from torch.autograd import Variable
-from torchvision.utils import save_image
-import torchvision.transforms as transforms
+from torch.cuda.amp import GradScaler, autocast
+from torchvision.utils import save_image, make_grid
 
-from PIL import Image
+from dataset import MNIST
+from networks.unet import UNet
 
-from network import Generator
-from dataset import MyDataset
-from zhenglin.dl.utils import weights_init_normal, LinearLambdaLR
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from tools.utils import LinearLambdaLR
 
 import wandb
-# wandb.init("")
+wandb.init(project="pytorch-template")
 
-parser = argparse.ArgumentParser()
-### dataset args
-parser.add_argument('--dataroot', type=str, default='', help='root directory of the dataset')
-parser.add_argument('--patch_size', type=int, default=256, help='size of the data crop (squared assumed)')
-parser.add_argument('--input_nc', type=int, default=3, help='number of channels of input data')
-parser.add_argument('--output_nc', type=int, default=3, help='number of channels of output data')
-parser.add_argument('--num_workers', type=int, default=8, help='number of cpu threads to use during batch generation')
-### training args
-parser.add_argument('--start_epoch', type=int, default=0, help='starting epoch')
-parser.add_argument('--end_epoch', type=int, default=200, help='number of epochs of training')
-parser.add_argument('--decay_epoch', type=int, default=100, help='epoch to start linearly decaying the learning rate to 0')
-parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
-parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate')
-parser.add_argument('--resume', action="store_true", help='continue training from a checkpoint')
-### advanced args
-parser.add_argument('--precision', type=str, default='fp32', choices=['fp32', 'fp16'], help='brutal precision of model')
-args = parser.parse_args()
+logging.basicConfig(level=logging.INFO)
 
-### set gpu device
-DEVICE = 0
+def main():
+    parser = argparse.ArgumentParser()
+    ### dataset args
+    parser.add_argument('--exp_root', type=str, default='expriments', help='root directory of the dataset')
+    parser.add_argument('--exp_name', type=str, default='', help='root directory of the dataset')
+    parser.add_argument('--patch_size', type=int, default=256, help='size of the data crop (squared assumed)')
+    parser.add_argument('--input_nc', type=int, default=1, help='number of channels of input data')
+    parser.add_argument('--output_nc', type=int, default=1, help='number of channels of output data')
+    parser.add_argument('--num_workers', type=int, default=8, help='number of cpu threads to use during batch generation')
+    parser.add_argument('--save_interval_epoch', type=int, default=1, help='interval epoch to save the model')
+    ### training args
+    parser.add_argument('--start_epoch', type=int, default=0, help='starting epoch')
+    parser.add_argument('--end_epoch', type=int, default=10, help='number of epochs of training')
+    parser.add_argument('--decay_epoch', type=int, default=5, help='epoch to start linearly decaying the learning rate to 0')
+    parser.add_argument('--batch_size', type=int, default=64, help='size of the batches')
+    parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate')
+    parser.add_argument('--resume', type=str, default='', help='continue training from a checkpoint')
+    parser.add_argument('--fp16', action="store_true", help='mixed precision of model')
+    parser.add_argument('--device', type=str, default='0', help='gpu index used')
+    args = parser.parse_args()
 
-### Networks
-model = Generator().to(DEVICE)
-if args.precision == 'fp16':    # Not encouraged. AMP is preferred, refer to ./train_amp.py
-    model = Generator().half().to(DEVICE)  # TODO:Set data to .half() as well
-model.apply(weights_init_normal)
-
-if args.resume:
-    model.load_state_dict(torch.load('./models/model_20.pth', map_location=torch.device(DEVICE)))
-
-### if rich
-# model = nn.DataParallel(model, device_ids=[0, 1])
-
-### Lossess
-criterion_GAN = torch.nn.MSELoss().to(DEVICE)
-
-### argsimizers & LR schedulers
-optimizer_G = torch.optim.Adam(model.parameters(), lr=args.lr, betas=(0.5, 0.999))
-
-lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(optimizer_G, lr_lambda=LinearLambdaLR(args.end_epoch, args.start_epoch, args.decay_epoch).step)
-
-### Inputs & targets memory allocation
-Tensor = torch.cuda.FloatTensor if args.precision=='fp32' else torch.cuda.HalfTensor
-input_A = Tensor(args.batch_size, args.input_nc, args.patch_size, args.patch_size)
-input_B = Tensor(args.batch_size, args.output_nc, args.patch_size, args.patch_size)
-target_real = Variable(Tensor(args.batch_size).fill_(1.0), requires_grad=False)
-target_fake = Variable(Tensor(args.batch_size).fill_(0.0), requires_grad=False)
-
-### Dataset loader
-transforms_ = transforms.Compose([
-                transforms.Resize(int(args.patch_size*1.12), Image.Resampling.BICUBIC), 
-                transforms.RandomCrop(args.patch_size), 
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)) ])
-dataset = MyDataset(args.dataroot, transforms_=transforms_, unaligned=True)
-dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
-
-###### Training ######
-for epoch in tqdm(range(args.start_epoch, args.end_epoch + 1)):
-    for i, batch in enumerate(dataloader):
-
-        # Input = Variable(input_A.copy_(batch))    ### style 1
-        Input = Variable(batch.type(Tensor)).to(DEVICE)   ### style 2
-
-        ###### Generator ######
-        optimizer_G.zero_grad()
-        
-        Pred = model(Input)
-        
-        loss_G = criterion_GAN(Input, Pred)
-
-        loss_G.backward()
-        optimizer_G.step()
-        
-        # wandb.log({"loss_G": loss_G.item()})
-        
-    # Update learning rates
-    lr_scheduler_G.step()
-
-    if epoch % 10 == 0:
-        save_image(Input, f'./imgs/{epoch}_real.jpg')
-        save_image(Pred, f'./imgs/{epoch}_fake.jpg')
+    DEVICE = 0
     
-    if epoch % 20 == 0:
-        torch.save(model.state_dict(), f'output/model_{epoch}.pth')
-###################################
+    ### save checkpoints
+    if args.exp_name:
+        dir_save = os.path.join(args.exp_root, args.exp_name)
+    else:
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        dir_save = os.path.join(args.exp_root, timestamp)
+
+    ### Dataset & DataLoader
+    dataset = MNIST()
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+
+    ### Networks
+    unet = UNet(chan_in=args.input_nc, chan_out=args.output_nc, long_skip=True).to(DEVICE)
+    
+    if args.resume:
+        unet.load_state_dict(torch.load(args.resume, map_location=torch.device(DEVICE)))
+
+    ### Lossess
+    criterion = torch.nn.MSELoss().to(DEVICE)
+
+    ### argsimizers & LR schedulers
+    optimizer = torch.optim.Adam(unet.parameters(), lr=args.lr, betas=(0.9, 0.999))
+    lr_scheduler = torch.optim.lr_scheduler.LambdaLR(
+        optimizer, lr_lambda=LinearLambdaLR(args.end_epoch * len(dataloader), 
+                                            args.start_epoch * len(dataloader), 
+                                            args.decay_epoch * len(dataloader)).step)
+    grad_scaler = GradScaler()
+
+    pbar = tqdm(total=len(dataloader) * (args.end_epoch - args.start_epoch))
+    
+    ###### Training ######
+    for epoch in range(args.start_epoch, args.end_epoch):
+        for i, batch in enumerate(dataloader):
+            image = batch['image'].to(DEVICE)
+            contour = batch['contour'].to(DEVICE)
+            
+            with autocast(enabled=args.fp16):
+                pred = unet(image)
+                
+                loss = criterion(pred, contour)
+                wandb.log({"train loss": loss.item(), "learning rate": float(lr_scheduler.get_lr()[-1])})
+
+                grad_scaler.scale(loss).backward()
+                grad_scaler.step(optimizer)
+                grad_scaler.update()
+                lr_scheduler.step()
+                optimizer.zero_grad()
+                pbar.update(1)
+
+        if epoch % args.save_interval_epoch == 0 or epoch == args.end_epoch - 1:
+            os.makedirs(os.path.join(dir_save, 'imgs'), exist_ok=True)
+            save_image(make_grid(pred, nrow=int(args.batch_size ** 0.5)), os.path.join(dir_save, 'imgs', f'{epoch}_pred.png'))
+            save_image(make_grid(image, nrow=int(args.batch_size ** 0.5)), os.path.join(dir_save, 'imgs', f'{epoch}_gt.png'))
+            save_image(make_grid(contour, nrow=int(args.batch_size ** 0.5)), os.path.join(dir_save, 'imgs', f'{epoch}_cond.png'))
+        
+            os.makedirs(os.path.join(dir_save, 'weights', f'epoch_{epoch}'), exist_ok=True)
+            torch.save(unet.state_dict(), os.path.join(dir_save, 'weights', f'epoch_{epoch}', 'unet.pth'))
+            logging.info(f"Epoch {epoch} is saved to {os.path.join(dir_save, 'weights', f'epoch_{epoch}', 'unet.pth')}")
+
+
+if __name__=="__main__":
+    main()
